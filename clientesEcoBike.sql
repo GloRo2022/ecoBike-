ef5306119c4b81ab597d4950e9262d90c382d053
GloRo2022 <laestaciondelasonrisa@gmail.com>
2021-08-31 10:01:47 -0500

    update clientes script

--- clientesEcoBike.sql
+++ clientesEcoBike.sql
@@ -4,16 +4,15 @@
 DROP TABLE IF EXISTS `mydb`.`Clientes EcoDosRuedas` ;
 
 SHOW WARNINGS;
-CREATE TABLE IF NOT EXISTS `mydb`.`Clientes EcoDosRuedas` (
-  `AliasCliente` VARCHAR(45) NOT NULL,
-  `Nombre` VARCHAR(20) NOT NULL,
-  `Apellido` VARCHAR(45) NULL,
-  `email` VARCHAR(45) NULL,
-  `Celular` INT NULL DEFAULT CURRENT_TIMESTAMP,
-  `Contraseña` INT NULL,
+CREATE TABLE IF NOT EXISTS `mydb`.`Clientes bicimoto` (
+  `alias` CHAR(20) NULL,
+  `nombre_apellido` CHAR(20) NULL,
+  `email` CHAR(45) NULL,
+  `Celular` INT (11) NULL,
+  `Contraseña` INT,
   `FechaNacimiento` DATETIME NULL,
   `IntenciónCompra_Registro` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`AliasCliente`, `IntenciónCompra_Registro`),
+  PRIMARY KEY (`Alias`),
   CONSTRAINT `fk_Clientes EcoDosRuedas_IntenciónCompra1`
     FOREIGN KEY (`IntenciónCompra_Registro`)
     REFERENCES `mydb`.`IntenciónCompra` (`Registro`)
@@ -21,7 +20,7 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Clientes EcoDosRuedas` (
     ON UPDATE NO ACTION);
 
 SHOW WARNINGS;
-CREATE INDEX `fk_Clientes EcoDosRuedas_IntenciónCompra1_idx` ON `mydb`.`Clientes EcoDosRuedas` (`IntenciónCompra_Registro` ASC) VISIBLE;
+
 
 SHOW WARNINGS;
 SELECT * FROM bicimoto.clientes;
